b1bfdd5b1f71072bfd775d4ab99c682c77584b36
Jedi77 <jprentup@hotmail.com>
2018-08-23 12:53:22 -0600

    No color still, links work

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+    window.open("https://github.com/rabbitattack/P1")
+}
+
+
+function myFunction1() {
+    window.open("https://github.com/rabbitattack/i-luv-beer")
+}
+
+
+function myFunction2() {
+    window.open("https://github.com/rabbitattack/P2")
+}
+
